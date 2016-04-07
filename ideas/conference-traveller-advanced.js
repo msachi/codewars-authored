4c56b7416cc46755d40a4029d7906f6fbb7e898e
Marko Sustarsic <marchee88@gmail.com>
2016-04-07 15:07:18 +0200

    Added files via upload

---
+++ ideas/conference-traveller-advanced.js
@@ -0,0 +1,54 @@
+/*
+This is an idea for an advanced version of the Conference Traveller kata.
+
+Lucy now uses three criteria to decide where to go (from most to least important):
+1) Conference in continent she hasn't been to
+2) Conference in city she hasn't been to
+3) Conference relevant to her field of research (citiesOffered already sorted)
+
+Find a conference for Lucy to go to.
+*/
+
+// Database of cities
+
+var cities = {
+	Australia: ['Melbourne','Sydney'],
+	NorthAmerica: ['NYC','San Francisco','LA','Chicago'],
+	SouthAmerica: ['Brasil','Rio','Buenos Aires'],
+	Europe: ['Berlin','Paris','London','Madrid'],
+	Africa: ['Johannesburg']
+};
+
+// Example input
+
+var citiesVisited = ['NYC', 'Rio', 'Johannesburg','Paris','London'];
+var citiesOffered = ['LA','Chicago'];
+
+// Solution
+
+function decideOnCity(citiesVisited,citiesOffered) {
+	for (var i = 0; i < citiesVisited.length; i++) {
+		for (var continent in cities) {
+		    var a = cities[continent].indexOf(citiesVisited[i]);
+			if (a !== -1) {
+				cities[continent].splice(a,a+1);
+				cities[continent].push("beenHere");
+			}
+		}
+	}
+	for (var j = 0; j < citiesOffered.length; j++) {
+	    for (var continent in cities) {
+	        if (cities[continent].indexOf("beenHere") == -1
+	        	&& cities[continent].indexOf(citiesOffered[j]) != -1)
+	        	return citiesOffered[j];
+	    }
+	}
+	for (var j = 0; j < citiesOffered.length; j++) {
+		for (var continent in cities) {
+			if (cities[continent].indexOf(citiesOffered[j]) != -1)
+				return citiesOffered[j];
+		}
+	}
+}
+
+// --> returns 'LA'
