f1003a1c3218dd4a620ee0ce0238b2d725684bbc
Marko Sustarsic <marchee88@gmail.com>
2016-04-07 14:49:16 +0200

    Update tests.js

--- conference-traveller/tests.js
+++ conference-traveller/tests.js
@@ -9,28 +9,28 @@ Test.assertEquals(conferencePicker(['London','Berlin','Mexico City','Melbourne',
 // Real tests
 
 Test.describe("Basic Tests", function() {
-	Test.assertEquals(conferencePicker(['Mexico City','Johannesburg','Stockholm','Osaka','Saint Petersburg','London'],['Stockholm','Paris','Melbourne']),'Paris');
-	Test.assertEquals(conferencePicker(['Buenos Aires','Mexico City','Johannesburg'],['Melbourne','Moscow']),'Melbourne');
-	Test.assertEquals(conferencePicker(['Tokyo','Madrid','Melbourne','Sydney','Rio De Janeiro','Saint Petersburg','Brisbane','Paris','Houston'],['Sydney','Chicago','Paris']),'Chicago');
-	Test.assertEquals(conferencePicker(['New York City','Johannesburg','Chicago'],['Johannesburg','Mumbai']),'Mumbai');
-	Test.assertEquals(conferencePicker(['Lisbon','San Francisco','Mexico City','Sydney','Mumbai','Brussels','Saint Petersburg','Osaka','Rio De Janeiro','Hong Kong','London','Paris'],['Saint Petersburg','Melbourne','New York City']),'Melbourne');
-	Test.assertEquals(conferencePicker(['New York City','Los Angeles','Houston','Moscow','Ankara','Philadelphia'],['Ankara','Osaka']),'Osaka');
-	Test.assertEquals(conferencePicker(['Mexico City','Sydney','Brussels','Saint Petersburg','Stockholm'],['Los Angeles','Singapore']),'Los Angeles');
-	Test.assertEquals(conferencePicker(['Brisbane','Delhi','Osaka','Johannesburg','Lisbon','Brussels','Houston','New York City','Seoul'],['San Francisco','Madrid']),'San Francisco');
-	Test.assertEquals(conferencePicker(['Brussels','Beijing','Paris','San Francisco','Sydney','Osaka','Brisbane','Melbourne','Ankara','Hong Kong','Los Angeles'],['Ankara','Madrid','Madrid']),'Madrid');
-	Test.assertEquals(conferencePicker(['Osaka','Brussels','Stockholm','Lisbon','Dubai','Tokyo','Ankara','London','Rio De Janeiro','Paris'],['Beijing','Osaka','Brisbane']),'Beijing');
+  Test.assertEquals(conferencePicker(['Mexico City','Johannesburg','Stockholm','Osaka','Saint Petersburg','London'],['Stockholm','Paris','Melbourne']),'Paris');
+  Test.assertEquals(conferencePicker(['Buenos Aires','Mexico City','Johannesburg'],['Melbourne','Moscow']),'Melbourne');
+  Test.assertEquals(conferencePicker(['Tokyo','Madrid','Melbourne','Sydney','Rio De Janeiro','Saint Petersburg','Brisbane','Paris','Houston'],['Sydney','Chicago','Paris']),'Chicago');
+  Test.assertEquals(conferencePicker(['New York City','Johannesburg','Chicago'],['Johannesburg','Mumbai']),'Mumbai');
+  Test.assertEquals(conferencePicker(['Lisbon','San Francisco','Mexico City','Sydney','Mumbai','Brussels','Saint Petersburg','Osaka','Rio De Janeiro','Hong Kong','London','Paris'],['Saint Petersburg','Melbourne','New York City']),'Melbourne');
+  Test.assertEquals(conferencePicker(['New York City','Los Angeles','Houston','Moscow','Ankara','Philadelphia'],['Ankara','Osaka']),'Osaka');
+  Test.assertEquals(conferencePicker(['Mexico City','Sydney','Brussels','Saint Petersburg','Stockholm'],['Los Angeles','Singapore']),'Los Angeles');
+  Test.assertEquals(conferencePicker(['Brisbane','Delhi','Osaka','Johannesburg','Lisbon','Brussels','Houston','New York City','Seoul'],['San Francisco','Madrid']),'San Francisco');
+  Test.assertEquals(conferencePicker(['Brussels','Beijing','Paris','San Francisco','Sydney','Osaka','Brisbane','Melbourne','Ankara','Hong Kong','Los Angeles'],['Ankara','Madrid','Madrid']),'Madrid');
+  Test.assertEquals(conferencePicker(['Osaka','Brussels','Stockholm','Lisbon','Dubai','Tokyo','Ankara','London','Rio De Janeiro','Paris'],['Beijing','Osaka','Brisbane']),'Beijing');
 });
 
 Test.describe("No cities visited", function() {
-	Test.assertEquals(conferencePicker([],['Philadelphia','Osaka','Tokyo','Melbourne']),'Philadelphia','Should work if no cities visited');
-	Test.assertEquals(conferencePicker([],['Brussels','Madrid','London']),'Brussels','Should work if no cities visited');
-	Test.assertEquals(conferencePicker([],['Sydney','Tokyo']),'Sydney','Should work if no cities visited');
+  Test.assertEquals(conferencePicker([],['Philadelphia','Osaka','Tokyo','Melbourne']),'Philadelphia','Should work if no cities visited');
+  Test.assertEquals(conferencePicker([],['Brussels','Madrid','London']),'Brussels','Should work if no cities visited');
+  Test.assertEquals(conferencePicker([],['Sydney','Tokyo']),'Sydney','Should work if no cities visited');
 });
 
 Test.describe("No new cities", function() {
-	Test.assertEquals(conferencePicker(['London','Berlin','Mexico City','Melbourne','Buenos Aires','Hong Kong','Madrid','Paris'],['Berlin','Melbourne']),'No worthwhile conferences this year!','Should work if all offered cities previously visited');
-	Test.assertEquals(conferencePicker(['Beijing','Johannesburg','Sydney','Philadelphia','Hong Kong','Stockholm','Chicago','Seoul','Mexico City','Berlin'],['Stockholm','Berlin','Chicago']),'No worthwhile conferences this year!','Should work if all offered cities previously visited');
-	Test.assertEquals(conferencePicker(['Mexico City','Dubai','Philadelphia','Madrid','Houston','Chicago','Delhi','Seoul','Mumbai','Lisbon','Hong Kong','Brisbane','Stockholm','Tokyo','San Francisco','Rio De Janeiro'],['Lisbon','Mexico City']),'No worthwhile conferences this year!','Should work if all offered cities previously visited');
+  Test.assertEquals(conferencePicker(['London','Berlin','Mexico City','Melbourne','Buenos Aires','Hong Kong','Madrid','Paris'],['Berlin','Melbourne']),'No worthwhile conferences this year!','Should work if all offered cities previously visited');
+  Test.assertEquals(conferencePicker(['Beijing','Johannesburg','Sydney','Philadelphia','Hong Kong','Stockholm','Chicago','Seoul','Mexico City','Berlin'],['Stockholm','Berlin','Chicago']),'No worthwhile conferences this year!','Should work if all offered cities previously visited');
+  Test.assertEquals(conferencePicker(['Mexico City','Dubai','Philadelphia','Madrid','Houston','Chicago','Delhi','Seoul','Mumbai','Lisbon','Hong Kong','Brisbane','Stockholm','Tokyo','San Francisco','Rio De Janeiro'],['Lisbon','Mexico City']),'No worthwhile conferences this year!','Should work if all offered cities previously visited');
 });
 
 Test.describe("Random Tests", function() {
@@ -38,11 +38,11 @@ Test.describe("Random Tests", function() {
   var cities = ['Ankara','Beijing','Berlin','Brisbane','Brussels','Buenos Aires','Chicago','Delhi','Dubai','Hong Kong','Houston','Johannesburg','Lisbon','London','Los Angeles','Madrid','Mexico City','Melbourne','Moscow','Mumbai','New York City','Osaka','Paris','Philadelphia','Rio De Janeiro','Saint Petersburg','San Francisco','Seoul','Singapore','Stockholm','Sydney','Tokyo']
   
   function testSolution (citiesVisited, citiesOffered) {
-	for (var i = 0; i < citiesOffered.length; i++) {
-		if (citiesVisited.indexOf(citiesOffered[i]) == -1)
-			return citiesOffered[i];
-	}
-	return 'No worthwhile conferences this year!';
+  for (var i = 0; i < citiesOffered.length; i++) {
+    if (citiesVisited.indexOf(citiesOffered[i]) == -1)
+      return citiesOffered[i];
+  }
+  return 'No worthwhile conferences this year!';
   }
 
   for (var j = 0; j < 100; j++) {  
